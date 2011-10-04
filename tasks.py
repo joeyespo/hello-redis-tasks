6494ed99ebca4b15a63aff15b3b15031240efd33
Joe Esposito <joe@joeyespo.com>
2011-10-03 20:30:32 -0400

    Limited the simulation time in tasks (since somebody was trying to add 99999 + 99999 on the demo :)

--- tasks.py
+++ tasks.py
@@ -4,6 +4,6 @@ from worker import delayable
 
 @delayable
 def add(x, y, delay=None):
-    # Simulate work, preferably something interesting so Python doesn't sleep
-    sleep(delay or x + y)
+    # Simulate your work here, preferably something interesting so Python doesn't sleep
+    sleep(delay or (x + y if 0 < x + y < 5 else 3))
     return x + y
